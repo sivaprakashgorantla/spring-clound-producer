2b73cf5abaf2f54c1e423a6bffc088955290996e
Sivaprakash Gorantla <sivaprakashgorantla@gmail.com>
2019-08-10 21:32:42 +0530

    employee-consumer

---
+++ employee-customer/src/main/java/com/springcloud/SpringBootHelloWorldApplicationConsumer.java
@@ -0,0 +1,31 @@
+package com.springcloud;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClientException;
+
+import com.springcloud.controllers.ConsumerControllerClient;
+
+@SpringBootApplication
+public class SpringBootHelloWorldApplicationConsumer {
+
+	public static void main(String[] args) throws RestClientException, IOException {
+		ApplicationContext ctx = SpringApplication.run(
+				SpringBootHelloWorldApplicationConsumer.class, args);
+		
+		ConsumerControllerClient consumerControllerClient=ctx.getBean(ConsumerControllerClient.class);
+		System.out.println(consumerControllerClient);
+		consumerControllerClient.getEmployee();
+		
+	}
+	
+	@Bean
+	public  ConsumerControllerClient  consumerControllerClient()
+	{
+		return  new ConsumerControllerClient();
+	}
+}
---
+++ employee-customer/src/main/java/com/springcloud/controllers/ConsumerControllerClient.java
@@ -0,0 +1,36 @@
+package com.springcloud.controllers;
+
+import java.io.IOException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+public class ConsumerControllerClient {
+
+	public void getEmployee() throws RestClientException, IOException {
+
+		String baseUrl = "http://localhost:8080/employee";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response=null;
+		try{
+			response=restTemplate.exchange(baseUrl,
+				HttpMethod.GET, getHeaders(),String.class);
+			System.out.println("Response Received"+response);
+			
+		}catch (Exception ex)
+		{
+			System.out.println("Exception : "+ex);
+		}
+		System.out.println(response.getBody());
+	}
+
+	private static HttpEntity<?> getHeaders() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new HttpEntity<>(headers);
+	}
+}
